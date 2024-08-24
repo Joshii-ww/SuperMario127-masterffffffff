d90a07988df60dacc22d1ea25396f0a35b54960a
Joshii-ww <70620833+Joshii-ww@users.noreply.github.com>
2024-08-24 14:48:29 +0200

    dadadadada
    the thing

--- scenes/menu/loading/loading.gd
+++ scenes/menu/loading/loading.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-onready var coins_text = $Text/Coins
-onready var coins_shadow = $Text/Coins/Shadow
+onready var coins_text = $Text / Coins
+onready var coins_shadow = $Text / Coins / Shadow
 
-onready var loading_text = $Text/Loading
-onready var loading_shadow = $Text/Loading/Shadow
+onready var loading_text = $Text / Loading
+onready var loading_shadow = $Text / Loading / Shadow
 
 onready var animation_player = $AnimationPlayer
 onready var luigi_frames = preload("res://scenes/actors/mario/luigi_frames.tres")
 
-onready var mario_sprite = $Sprites/Player/AnimatedSprite
-onready var mario_reflection = $Sprites/Player/Reflection
+onready var mario_sprite = $Sprites / Player / AnimatedSprite
+onready var mario_reflection = $Sprites / Player / Reflection
 
 onready var coin_sound = $CoinSound
 onready var coin_scene = preload("res://scenes/menu/loading/coin.tscn")
@@ -32,18 +32,18 @@ var done = false
 
 func go_to_menu():
 	while Singleton.EditorSavedSettings.loading_tileset:
-		yield(get_tree(), "idle_frame")
+		yield (get_tree(), "idle_frame")
 	
-	if !Singleton.loaded:
+	if not Singleton.loaded:
 		Singleton.loaded = true
 		var number_of_tiles = load("res://assets/tiles/tiles.tres").get_last_unused_tile_id()
-		if !(number_of_tiles == Singleton.EditorSavedSettings.data_tiles and ResourceLoader.exists("user://tiles.res", "TileSet")):
+		if not (number_of_tiles == Singleton.EditorSavedSettings.data_tiles and ResourceLoader.exists("user://tiles.res", "TileSet")):
 			Singleton.EditorSavedSettings.tileset_loaded = true
 			Singleton.EditorSavedSettings.data_tiles = number_of_tiles
 			var _reload_scene = get_tree().reload_current_scene()
-		else:
+		else :
 			Singleton.MenuVariables.quit_to_menu()
-	else:
+	else :
 		Singleton.MenuVariables.quit_to_menu()
 		
 static func get_data_or_null():
@@ -55,7 +55,7 @@ static func get_data_or_null():
 	file.close()
 	if typeof(data) == TYPE_DICTIONARY:
 		return data
-	else:
+	else :
 		return null
 		
 func _ready():
@@ -66,11 +66,11 @@ func _ready():
 		if data.has("darkmode"):
 			Singleton2.dark_mode = data["darkmode"]
 	if Singleton2.dark_mode:
-		$Background/ColorRect.visible = false
-		$Background/TextureRect.visible = false
-		$Text/Loading.modulate = Color(1,1,1)
-		$Text/Coins.modulate = Color(1,1,1)
-		$Foreground/ColorRect.color = Color(0,0,0,1)
+		$Background / ColorRect.visible = false
+		$Background / TextureRect.visible = false
+		$Text / Loading.modulate = Color(1, 1, 1)
+		$Text / Coins.modulate = Color(1, 1, 1)
+		$Foreground / ColorRect.color = Color(0, 0, 0, 1)
 	dir.make_dir("user://replays")
 	dir.make_dir("user://autosave")
 	if file.file_exists("user://072.json"):
@@ -81,22 +81,14 @@ func _ready():
 		file.open("user://080.darius", File.WRITE)
 		file.close()
 		dir.remove("user://072.json")
-	else:
+	else :
 		file.open("user://080.darius", File.WRITE)
 		file.close()
-	if Singleton2.rp == true:
-		update_activity()
-	elif Singleton2.rp == false:
-		if Singleton2.dead == false:
-			Discord.queue_free()
-			Singleton2.dead = true
-		elif Singleton2.dead == true:
-			pass
 	animation_player.play("FadeIn")
-#	yield(get_tree().create_timer(5), "timeout")
-	if !Singleton.loaded:
+
+	if not Singleton.loaded:
 		thread.start(self, "load_singletons", null, thread.PRIORITY_HIGH)
-	else:
+	else :
 		loading_text.text = "Creating tile data..."
 		loading_shadow.text = loading_text.text
 		
@@ -131,27 +123,9 @@ func _physics_process(delta):
 	if spawn_timer > 0:
 		spawn_timer -= delta
 
-func update_activity() -> void:
-	var activity = Discord.Activity.new()
-	activity.set_type(Discord.ActivityType.Playing)
-	activity.set_state("Loading...")
-
-	var assets = activity.get_assets()
-	assets.set_large_image("sm127")
-	assets.set_large_text("0.8.0")
-	assets.set_small_image("capsule_main")
-	assets.set_small_text("ZONE 2 WOOO")
-	
-	var timestamps = activity.get_timestamps()
-	timestamps.set_start(OS.get_unix_time() + 1)
-
-	var result = yield(Discord.activity_manager.update_activity(activity), "result").result
-	if result != Discord.Result.Ok:
-		push_error(str(result))
-		
 
 func load_palettes(_userdata):
-	var level_tilesets := preload("res://assets/tiles/ids.tres")
+	var level_tilesets: = preload("res://assets/tiles/ids.tres")
 	var tileset_resource = preload("res://assets/tiles/tiles.tres")
 	var number_of_tiles = tileset_resource.get_last_unused_tile_id()
 	
@@ -161,23 +135,23 @@ func load_palettes(_userdata):
 	Singleton.EditorSavedSettings.tileset_loaded = true
 	
 	for tileset_id in level_tilesets.ids:
-		var tileset : LevelTileset = load("res://assets/tiles/" + tileset_id + "/resource.tres")
+		var tileset:LevelTileset = load("res://assets/tiles/" + tileset_id + "/resource.tres")
 		print("Setting up tile ", str(current_index), " - ", tileset.palettes.size(), " palettes")
 		var tile_variations = [
-			tileset.block_tile_id,
-			tileset.slab_tile_id,
-			tileset.left_slope_tile_id,
+			tileset.block_tile_id, 
+			tileset.slab_tile_id, 
+			tileset.left_slope_tile_id, 
 			tileset.right_slope_tile_id
 		]
 		
 		var palette_ids = []
 		palette_ids.resize(tileset.palettes.size())
-		var palette_ids_i := 0
+		var palette_ids_i: = 0
 		for palette in tileset.palettes:
 			var tile_variation_ids = []
 			tile_variation_ids.resize(tile_variations.size())
 			print(tile_variations)
-			var tile_variations_i := 0
+			var tile_variations_i: = 0
 			for base_tile_id in tile_variations:
 				var new_tile_id = tileset_resource.get_last_unused_tile_id()
 				tileset_resource.create_tile(new_tile_id)
@@ -186,10 +160,10 @@ func load_palettes(_userdata):
 				tileset_resource.autotile_set_size(new_tile_id, tileset_resource.autotile_get_size(base_tile_id))
 				tileset_resource.tile_set_tile_mode(new_tile_id, tileset_resource.tile_get_tile_mode(base_tile_id))
 				
-				var region =  tileset_resource.tile_get_region(base_tile_id)
+				var region = tileset_resource.tile_get_region(base_tile_id)
 				for coord_x in range(region.size.x):
 					for coord_y in range(region.size.y):
-						var coord := Vector2(coord_x, coord_y)
+						var coord: = Vector2(coord_x, coord_y)
 						tileset_resource.autotile_set_bitmask(new_tile_id, coord, tileset_resource.autotile_get_bitmask(base_tile_id, coord))
 				
 				tileset_resource.tile_set_region(new_tile_id, region)
@@ -218,9 +192,9 @@ func load_palettes(_userdata):
 	print("Finished loading all " + str(level_tilesets.ids.size()) + " tilesets.")
 
 func load_singletons(_userdata):
-	var ms_start := OS.get_ticks_msec()
+	var ms_start: = OS.get_ticks_msec()
 	
-	# First 25% = load singletons
+	
 	for scene in Singleton.load_paths:
 		resource_loader = ResourceLoader.load_interactive(scene[1])
 		
@@ -241,13 +215,13 @@ func load_singletons(_userdata):
 		percentage = float(current_index) / float(amount_of_scenes) * 0.25
 		print("Instanced scene " + str(current_index))
 	
-	var ms_end := OS.get_ticks_msec()
+	var ms_end: = OS.get_ticks_msec()
 	print("Finished loading all " + str(amount_of_scenes) + " singletons in ", str(ms_end - ms_start), " ms")
 	
-	# Last 75% = load scenes within the singletons
-	var loading_nodes := [Singleton.MiscCache, Singleton.CurrentLevelData]
-	var loaded_ids_sum := 0
-	var loaded_max_sum := 1 # to be able to enter the loop
+	
+	var loading_nodes: = [Singleton.MiscCache, Singleton.CurrentLevelData]
+	var loaded_ids_sum: = 0
+	var loaded_max_sum: = 1
 	
 	while loaded_ids_sum < loaded_max_sum:
 		OS.delay_msec(1)
@@ -258,15 +232,15 @@ func load_singletons(_userdata):
 			loaded_ids_sum += node.loaded_ids
 			loaded_max_sum += node.loaded_ids_max
 		
-		#var previous_percentage = percentage
+		
 		percentage = 0.25 + (float(loaded_ids_sum) / float(loaded_max_sum) * 0.75)
-		#if previous_percentage != percentage:
-		#	print(loaded_ids_sum, " / ", loaded_max_sum, " loaded")
+		
+		
 	
 	done = true
 	
-	#used to display the skip button once all singletons have loaded
-	if(done == true):
+	
+	if (done == true):
 		animation_player.play("buttonIn")
 	percentage = 1
 	print("Finished loading all scenes within the singletons")
--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -1,39 +1,32 @@
 extends Screen
 
-onready var button_campaign : Button = $Panel/VBoxContainer/ButtonCampaign
-onready var button_levels : Button = $Panel/VBoxContainer/ButtonLevels
-onready var button_search : Button = $Panel/VBoxContainer/ButtonSearch
-onready var button_templates : Button = $Panel/VBoxContainer/ButtonTemplates
-onready var button_options : Button = $Panel/VBoxContainer/ButtonOptions
-onready var button_quit : Button = $Panel/ButtonQuit
-onready var button_login : Button = $Panel/ButtonLogin
-onready var button_skip = $Control/Skip
-onready var button_speed = $Control/Speed
-onready var credits = $Control/AnimationPlayer
-onready var credits2 = $Control/AnimationPlayer2
-onready var credits3 = $Control/AnimationPlayer3
+onready var button_campaign:Button = $Panel / VBoxContainer / ButtonCampaign
+onready var button_levels:Button = $Panel / VBoxContainer / ButtonLevels
+onready var button_search:Button = $Panel / VBoxContainer / ButtonSearch
+onready var button_templates:Button = $Panel / VBoxContainer / ButtonTemplates
+onready var button_options:Button = $Panel / VBoxContainer / ButtonOptions
+onready var button_quit:Button = $Panel / ButtonQuit
+onready var button_login:Button = $Panel / ButtonLogin
+onready var button_skip = $Control / Skip
+onready var button_speed = $Control / Speed
+onready var credits = $Control / AnimationPlayer
+onready var credits2 = $Control / AnimationPlayer2
+onready var credits3 = $Control / AnimationPlayer3
 onready var error_window = $ErrorWindow
 
 onready var timer = $CooldownTimer
 
 onready var lss_icon = preload("res://assets/misc/LSS.svg")
 
-const EDITOR_SCENE : PackedScene = preload("res://scenes/editor/editor.tscn")
+const EDITOR_SCENE:PackedScene = preload("res://scenes/editor/editor.tscn")
 
 
-func _ready() -> void:
-	$Control/ColorRect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+func _ready()->void :
+	$Control / ColorRect.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	if UserInfo.username != "":
 		button_login.text = "Logged in as " + UserInfo.username
 	Singleton2.crash = false
-	if Singleton2.rp == true:
-		update_activity()
-	elif Singleton2.rp == false:
-		if Singleton2.dead == false:
-			Discord.queue_free()
-			Singleton2.dead = true
-		elif Singleton2.dead == true:
-			pass
+	
 	var _connect = button_levels.connect("pressed", self, "on_button_levels_pressed")
 	_connect = button_templates.connect("pressed", self, "on_button_templates_pressed")
 	_connect = button_options.connect("pressed", self, "on_button_options_pressed")
@@ -44,31 +37,14 @@ func _ready() -> void:
 	_connect = button_skip.connect("pressed", self, "on_button_skip_pressed")
 	_connect = button_speed.connect("pressed", self, "on_button_speed_pressed")
 	
-#func _on_button_login_pressed():
-#	$LogInWindow.open()
-#
-func update_activity() -> void:
-	var activity = Discord.Activity.new()
-	activity.set_type(Discord.ActivityType.Playing)
-	activity.set_state("On the Main Menu")
-
-	var assets = activity.get_assets()
-	assets.set_large_image("sm127")
-	assets.set_large_text("0.8.0")
-	assets.set_small_image("capsule_main")
-	assets.set_small_text("ZONE 2 WOOO")
-	
-	var timestamps = activity.get_timestamps()
-	timestamps.set_start(OS.get_unix_time() + 1)
-
-	var result = yield(Discord.activity_manager.update_activity(activity), "result").result
-	if result != Discord.Result.Ok:
-		push_error(str(result))
 
 
-func _input(_event : InputEvent) -> void:
-	if !can_interact or get_focus_owner() != null:
-		return
+
+
+
+func _input(_event:InputEvent)->void :
+	if not can_interact or get_focus_owner() != null:
+		return 
 	
 	if Input.is_action_just_pressed("ui_up"):
 		button_quit.grab_focus()
@@ -79,67 +55,67 @@ func _input(_event : InputEvent) -> void:
 	elif Input.is_action_just_pressed("ui_right"):
 		pass
 
-func on_button_search_pressed() -> void:
+func on_button_search_pressed()->void :
 	emit_signal("screen_change", "main_menu_screen", "search_screen")
 	
 func _process(delta):
 	pass
 	
-func on_button_levels_pressed() -> void:
+func on_button_levels_pressed()->void :
 	if timer.time_left > 0:
-		return
+		return 
 	if Singleton.SavedLevels.is_template_list:
 		Singleton.SavedLevels.is_template_list = false
-		# Prevents errors when swapping between level lists
+		
 		Singleton.SavedLevels.selected_level = Singleton.SavedLevels.NO_LEVEL
 	timer.start()
 	emit_signal("screen_change", "main_menu_screen", "levels_screen")
 
-func on_button_templates_pressed() -> void:
+func on_button_templates_pressed()->void :
 	if timer.time_left > 0:
-		return
-	if !Singleton.SavedLevels.is_template_list:
+		return 
+	if not Singleton.SavedLevels.is_template_list:
 		Singleton.SavedLevels.is_template_list = true
-		# Prevents errors when swapping between level lists
+		
 		Singleton.SavedLevels.selected_level = Singleton.SavedLevels.NO_LEVEL
 	timer.start()
 	emit_signal("screen_change", "main_menu_screen", "levels_screen")
 
-func on_button_options_pressed() -> void:
+func on_button_options_pressed()->void :
 	if timer.time_left > 0:
-		return
-	if !Singleton.SavedLevels.is_template_list:
+		return 
+	if not Singleton.SavedLevels.is_template_list:
 		Singleton.SavedLevels.is_template_list = true
-		# Prevents errors when swapping between level lists
+		
 		Singleton.SavedLevels.selected_level = Singleton.SavedLevels.NO_LEVEL
 	timer.start()
 	emit_signal("screen_change", "main_menu_screen", "options_screen")
 
-func on_button_quit_pressed() -> void:
+func on_button_quit_pressed()->void :
 	get_tree().quit()
 	
-func on_button_speed_pressed() -> void:
+func on_button_speed_pressed()->void :
 	if credits.playback_speed == 1:
 		button_speed.text = "Speed (1x)"
 		credits.playback_speed = 5
-	else:
+	else :
 		button_speed.text = "Speed (5x)"
 		credits.playback_speed = 1
 		
-func on_button_skip_pressed() -> void:
+func on_button_skip_pressed()->void :
 	Singleton.Music.stop_temporary_music()
 	credits.play("skip")
-	$Control/ColorRect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	$Control / ColorRect.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	
-func on_button_credits_pressed() -> void:
+func on_button_credits_pressed()->void :
 	Singleton.Music.play_temporary_music(66, 50)
 	credits3.play("Pasted Animation")
 	credits.play("roll")
 	var _connect = credits.connect("animation_finished", self, "on_roll_finished")
 	credits2.play("button in")
-	$Control/ColorRect.mouse_filter = Control.MOUSE_FILTER_STOP
+	$Control / ColorRect.mouse_filter = Control.MOUSE_FILTER_STOP
 
 func on_roll_finished(anim_name):
 	if anim_name == "roll":
 		Singleton.Music.stop_temporary_music()
-		$Control/ColorRect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		$Control / ColorRect.mouse_filter = Control.MOUSE_FILTER_IGNORE
